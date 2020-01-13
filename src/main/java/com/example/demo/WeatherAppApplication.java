8f10672fb9f1589ae12e06addba66061a38f58e0
andrei <andrei>
2020-01-13 22:20:41 +0200

    tested endpoint of app

--- src/main/java/com/example/demo/WeatherAppApplication.java
+++ src/main/java/com/example/demo/WeatherAppApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.InputStream;
+import java.net.URLConnection;
 
 @SpringBootApplication
 public class WeatherAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherAppApplication.class, args);
-	}
 
+		HttpResponse<String> httpResponse = Unirest.get("https://api.weather.gov/gridpoints/TOP/31,80")
+       .header("x-rapidapi-key", "c2e075a4f9msh7dcde5e254e52c0p1a1641jsn91f26824d44")
+       .asString();
+		System.out.println(httpResponse.getBody());
+	}
+	
 }
